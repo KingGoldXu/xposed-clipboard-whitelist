27ae1a832a83fda405077a90564e8760125b1225
KingXu <857442867@qq.com>
2024-01-15 01:58:55 +0800

    allow BGClipboard by hooking isDefaultIme (v1.2)

--- app/src/main/java/sh/tablet/bgclipboard/BGClipboard.java
+++ app/src/main/java/sh/tablet/bgclipboard/BGClipboard.java
@@ -1,35 +1,52 @@
 package sh.tablet.bgclipboard;
 
-import static de.robv.android.xposed.XposedHelpers.*;
-
 import android.util.Log;
-import java.lang.String;
-import java.lang.reflect.Method;
 
-import de.robv.android.xposed.*;
+import java.util.Arrays;
+import java.util.List;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class BGClipboard implements IXposedHookLoadPackage {
-    static String logtag = "===tablettttttt===";
+    private static final String logTag = "===tablettttttt===";
+    private static final List<String> allowedPackages = Arrays.asList(
+            "com.fooview.android.fooview",
+            "com.baidu.input_oppo",
+            "com.baidu.input",
+            "com.iflytek.inputmethod",
+            "com.google.android.inputmethod.pinyin",
+            "com.sohu.inputmethod.sogou");
+    private static final String clipboardService = "com.android.server.clipboard.ClipboardService";
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-
         if(!lpparam.packageName.equals("android")) {
             return;
         }
-        Log.d(BGClipboard.logtag, "loading " + lpparam.packageName);
-        Log.d(BGClipboard.logtag, "proc = " + lpparam.processName);
-        XposedBridge.log(BGClipboard.logtag + " - Loaded app: " + lpparam.packageName);
-        Class<?> cb = findClass("com.android.server.clipboard.ClipboardService",lpparam.classLoader);
-        Method[] m = cb.getDeclaredMethods();
-        for (Method method : m) {
-            Log.d(BGClipboard.logtag, "... " + method.getName());
-            if (method.getName().contains("Allowed") && !method.getName().contains("$")) {
-                XposedBridge.hookMethod(method, XC_MethodReplacement.returnConstant(10000, true));
-                Log.d(BGClipboard.logtag, "found it!!!!! " + method.getName());
-            }
-        }
+
+        XposedHelpers.findAndHookMethod(clipboardService,
+                lpparam.classLoader,
+                "isDefaultIme",
+                int.class,
+                String.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        try {
+                            Log.d(logTag, "hook before isDefaultIme(int, String)");
+                            String packageName = (String) param.args[1];
+                            if (allowedPackages.contains(packageName)) {
+                                param.setResult(true);
+                            } else {
+                                param.setResult(false);
+                            }
+                        } catch (Throwable t) {
+                            Log.e(logTag, "Failed to override isDefaultIme(int, String)", t);
+                        }
+                    }
+                });
     }
 }
